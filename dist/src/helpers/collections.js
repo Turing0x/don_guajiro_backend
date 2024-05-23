be893d50bf7dfe5ea1309f74352dbde15963ec5e
Turing0x <83433135+Turing0x@users.noreply.github.com>
2024-05-23 15:31:12 -0400

    compilado y todo

---
+++ dist/src/helpers/collections.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.COLLECTIONS = void 0;
+exports.COLLECTIONS = {
+    USERS: 'users',
+    DEBTS: 'debts',
+    SALES: 'sales'
+};
---
+++ dist/src/routes/index.routes.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.api = void 0;
+const express_1 = __importDefault(require("express"));
+exports.api = express_1.default.Router();
+const collections_1 = require("../helpers/collections");
+const user_router_1 = require("../api/Users/router/user.router");
+const debts_router_1 = require("../api/Debts/router/debts.router");
+const sales_router_1 = require("../api/Sale/router/sales.router");
+exports.api.use(`/${collections_1.COLLECTIONS.USERS}`, user_router_1.UsersRouter);
+exports.api.use(`/${collections_1.COLLECTIONS.DEBTS}`, debts_router_1.DebtsRouter);
+exports.api.use(`/${collections_1.COLLECTIONS.SALES}`, sales_router_1.SalesRouter);
